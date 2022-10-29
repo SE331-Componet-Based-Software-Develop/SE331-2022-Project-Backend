f8775b81991f898349094b898790e0aaa2945ae4
MirrorML <2867405686@qq.com>
2022-10-29 21:40:16 +0800

    add auth

---
+++ src/main/java/se331/rest/entity/DoctorAuthDTO.java
@@ -0,0 +1,8 @@
+package se331.rest.entity;/**
+ * @author  jmm
+ * @date  2022/10/29 20:43
+ * @version 1.0
+ * @email 2867405686@qq.com
+ */
+public class DoctorAuthDTO {
+}
---
+++ src/main/java/se331/rest/entity/PatientAuthDTO.java
@@ -0,0 +1,8 @@
+package se331.rest.entity;/**
+ * @author  jmm
+ * @date  2022/10/29 21:06
+ * @version 1.0
+ * @email 2867405686@qq.com
+ */
+public class PatientAuthDTO {
+}
