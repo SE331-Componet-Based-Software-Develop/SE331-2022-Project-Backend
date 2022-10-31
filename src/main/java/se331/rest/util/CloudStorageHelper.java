8513bc23529c929d2f54e0ae4f1d78b7a0ee5580
MirrorML <2867405686@qq.com>
2022-10-31 12:40:39 +0800

    test

--- src/main/java/se331/rest/util/CloudStorageHelper.java
+++ src/main/java/se331/rest/util/CloudStorageHelper.java
@@ -20,59 +20,54 @@ import java.util.Date;
 
 @Component
 public class CloudStorageHelper {
-    private static Storage storage = null;
+    private static Storage storage=null;
     static {
-
-        InputStream serviceAccount =
-                null;
-        try {
-            serviceAccount = new ClassPathResource("upload.json").getInputStream();
+        InputStream serviceAccount=null;
+        try{
+            serviceAccount=new ClassPathResource("firebase-key.json").getInputStream();
             storage = StorageOptions.newBuilder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .setProjectId("projectimageupload-96213")
+                    .setProjectId("imageupload-64688")
                     .build().getService();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
     }
-    // [END init]
-
-    public String uploadFile(MultipartFile filePart, final String bucketName) throws IOException {
-
-
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HHmmssSSS");
-        String dtString = sdf.format(new Date());
-        final String fileName = dtString + "-" +filePart.getOriginalFilename();
+    public String uploadFile(MultipartFile filePart,final String bucketName) throws IOException{
+        SimpleDateFormat sdf = new SimpleDateFormat ( "yyyy-MM-dd HHmmssSSS" ) ;
+        String dtString = sdf.format (new Date());
+        final String fileName = dtString + "-"
+                +filePart.getOriginalFilename();
         InputStream is = filePart.getInputStream();
         ByteArrayOutputStream os = new ByteArrayOutputStream();
-        byte[] readBuf = new byte[4096];
-        while (is.available() > 0) {
-            int bytesRead = is.read(readBuf);
-            os.write(readBuf, 0, bytesRead);
+        byte[] readBuf = new byte [ 4096];
+        while (is.available () > 0) {
+            int bytesRead = is.read (readBuf);
+            os.write (readBuf, 0, bytesRead) ;
         }
-        // Convert ByteArrayOutputStream into byte[]
+        //Convert ByteArrayoutputstream into byte[ ]
         BlobInfo blobInfo =
-                storage.create(
+                storage. create (
                         BlobInfo
-                                .newBuilder(bucketName, fileName)
-                                // Modify access list to allow all users with link to read file
-                                .setAcl(new ArrayList<>(Arrays.asList(Acl.of(Acl.User.ofAllUsers(), Acl.Role.READER))))
-                                .build(),
-                        os.toByteArray());
-        // return the public download link
-        return blobInfo.getMediaLink();
+                                . newBuilder (bucketName, fileName )
+                                // Modify access list to allow all users withlink to read file
+                                .setAcl (new
+                                        ArrayList<>(Arrays.asList(Acl.of(Acl.User.ofAllUsers(), Acl.Role. READER))) )
+                                .build () ,
+                        os.toByteArray () ) ;
+        //return the public download link
+        return blobInfo.getMediaLink ( ) ;
+
     }
-    public String getImageUrl(MultipartFile file,final String bucket) throws IOException, ServletException {
-        final String fileName = file.getOriginalFilename();
-        // Check extension of file
-        if (fileName != null && !fileName.isEmpty() && fileName.contains(".")) {
-            final String extension = fileName.substring(fileName.lastIndexOf('.') + 1);
-            String[] allowedExt = {"jpg", "jpeg", "png", "gif"};
-            for (String s : allowedExt) {
-                if (extension.equals(s)) {
-                    return this.uploadFile(file, bucket);
+    public String getImageUrl(MultipartFile file,final  String bucket) throws IOException, ServletException {
+        final  String fileName=file.getOriginalFilename();
+        if(fileName!=null && !fileName.isEmpty() && fileName.contains("."))
+        {
+            final String extension =fileName.substring(fileName.lastIndexOf('.')+1);
+            String[] allowedExt={"jpg","jpeg","png","gif"};
+            for (String s:allowedExt){
+                if (extension.equals(s)){
+                    return this.uploadFile(file,bucket);
                 }
             }
             throw new ServletException("file must be an image");
@@ -80,4 +75,3 @@ public class CloudStorageHelper {
         return null;
     }
 }
-
